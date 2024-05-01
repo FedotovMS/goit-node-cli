687d1bbc3e5c8943728ea03eaa768615aae3cea6
Mykola Fedotov <fedotovns@gmail.com>
2024-05-01 12:35:30 +0200

    done

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import { program } from "commander";
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} from "./contacts.js";
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -10,23 +16,34 @@ program.parse();
 
 const options = program.opts();
 
-// TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
+      const contacts = await listContacts();
+      console.table(contacts);
       break;
 
     case "get":
-      // ... id
+      const contact = await getContactById(id);
+      if (!contact) {
+        console.log(`Contact with id=${id} not found`);
+        break;
+      }
+      console.log(contact);
       break;
 
     case "add":
-      // ... name email phone
+      const newContact = await addContact(name, email, phone);
+      console.log(newContact);
       break;
 
     case "remove":
-      // ... id
+      const deletedContact = await removeContact(id);
+      if (!deletedContact) {
+        console.log(`Contact with id=${id} not found`);
+        break;
+      }
+      console.log(deletedContact);
       break;
 
     default:
